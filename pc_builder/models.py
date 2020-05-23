3b7116e5c97b5ed98a2a41effd964c416c5663ac
smuchy <nikola.ristic14@gmail.com>
2020-05-23 20:15:07 +0200

    Registration, login and logout finalized.

--- pc_builder/models.py
+++ pc_builder/models.py
@@ -7,26 +7,35 @@ import uuid
 
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "test"))
 
+
 class User:
     def __init__(self, username):
         self.username = username
 
     def find(self):
-        user = graph.run("MATCH (n:User { username: '%s'} ) RETURN n.username" %self.username).data()
+        user = graph.run(
+            "MATCH (n:User { username: '%s'} ) RETURN n" % self.username
+        ).data()
         return user
 
-    def register(self, password):
+    def register(self, password, first_name, last_name, email):
         if not self.find():
-           user = Node('User', username=self.username, password=bcrypt.encrypt(password))
-           graph.create(user)
-           return True
+            user = Node(
+                "User",
+                username=self.username,
+                password=bcrypt.encrypt(password),
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+            )
+            graph.create(user)
+            return True
         else:
-           return False
+            return False
 
     def verify_password(self, password):
         user = self.find()
         if user:
-            return bcrypt.verify((password, user['password']))
+            return bcrypt.verify(password, user[0]["n"]["password"])
         else:
             return False
-
--- pc_builder/views.py
+++ pc_builder/views.py
@@ -1,29 +1,59 @@
 from .models import User
-from flask import Flask, request, session, redirect, url_for, render_template, flash, jsonify
+from flask import (
+    Flask,
+    request,
+    session,
+    redirect,
+    url_for,
+    render_template,
+    flash,
+    jsonify,
+)
 
 app = Flask(__name__)
 
-@app.route("/register", methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.get_json()['username']
-        password = request.get_json()['password']
+    if request.method == "POST":
+        username = request.get_json()["username"]
+        password = request.get_json()["password"]
+        first_name = request.get_json()["first_name"]
+        last_name = request.get_json()["last_name"]
+        email = request.get_json()["email"]
 
-    if not User(username).register(password):
-        return jsonify(response = "fail")
+    if not User(username).register(password, first_name, last_name, email):
+        return jsonify(response="fail")
     else:
-        session['username'] = username
-        return jsonify(response = "success")
+        session["username"] = username
+        return jsonify(response="success")
 
-@app.route("/login", methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.get_json()['username']
-        password = request.get_json()['password']
+    if request.method == "POST":
+        username = request.get_json()["username"]
+        password = request.get_json()["password"]
 
     if not User(username).verify_password(password):
-        return jsonify(response = "fail")
+        return jsonify(response="fail")
     else:
-        session['username'] = username
-        return jsonify(response = "success")
-    
+        session["username"] = username
+        return jsonify(response="success")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    flash("Logged out.")
+    return "success"
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    logged_in_user = session.get("username")
+
+    if logged_in_user:
+        return "Ovo je profil hehe"
+    else:
+        return "Please login"
